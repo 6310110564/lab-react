caf627bdf43188ff56d937fc8af7f07d9fc5da74
Aritsara Niyomdecha <aritsara.me2545@gmail.com>
2022-08-19 14:08:16 +0700

    Part 7 (2nd time)

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 import WordCard from './WordCard';
 
 const word = "Hello";
-const word1 = "World"
-const word2 = "Blue"
+const word1 = "World";
+const word2 = "Blue";
+const word3 = "Sky";
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
       <WordCard value={word}/>
       <WordCard value={word1}/>
       <WordCard value={word2}/>
-
+      <WordCard value={word3}/>
     </div>
   );
 }
