bac85e85071e3e070f16b8c5d152cde37e24a2a7
Aritsara Niyomdecha <aritsara.me2545@gmail.com>
2022-08-19 12:33:18 +0700

    part 7 (1st time)

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import './App.css';
 import WordCard from './WordCard';
 
 const word = "Hello";
+const word1 = "World"
+const word2 = "Blue"
+
 function App() {
   return (
     <div>
+      <h1>Hello Welcome to WordCard Game</h1>
+      <h2>เรียงลำดับข้อความให้ถูกต้อง</h2>
       <WordCard value={word}/>
+      <WordCard value={word1}/>
+      <WordCard value={word2}/>
+
     </div>
   );
 }
