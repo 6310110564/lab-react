17cdd4668187765348e8c6c364c22aa4de219841
Aritsara Niyomdecha <aritsara.me2545@gmail.com>
2022-08-19 15:41:16 +0700

    Part 7 (5th time)

--- src/Hint.js
+++ src/Hint.js
@@ -10,4 +10,12 @@ class Hint extends React.Component{
           hint: "โลกสีฟ้าทักทายน้ำเงิน"
         })
       }  
+      render(){
+        return (
+        <div>
+            <button type="button" onClick={this.showhint} >Hint Click!!</button>
+            <p>{this.state.hint}</p>
+        </div>
+        );
+      }
 }
